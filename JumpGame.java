4a98fc4cbf4a4295b45d0ed78679d1dda166fda1
mohinimehrotra <mohinimehrotra123@gmail.com>
2024-08-05 23:59:21 +0530

    Jump Game

--- JumpGame.java
+++ JumpGame.java
@@ -20,7 +20,7 @@ public class JumpGame {
 		int[] nums1 = {2,3,1,1,4};		
 		int[] nums2 = {3,2,1,0,4};
 		System.out.println("Nums1 can reach last index?"+ canJump(nums1));
-		System.out.println("Nums1 can reach last index?"+ canJump(nums2));
+		System.out.println("Nums2 can reach last index?"+ canJump(nums2));
 
 	}
 
